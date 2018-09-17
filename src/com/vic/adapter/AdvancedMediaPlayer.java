535e1b8fd3367aaf29993473a9c7d3f0c7469f57
Faithlmy <lmengyy@126.com>
2018-09-17 10:52:45 +0800

    delete

--- src/com/vic/adapter/AdvancedMediaPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.vic.adapter;
-
-public interface AdvancedMediaPlayer {
-
-	public void playVlc(String name);
-	public void playMp4(String name);
-}
--- src/com/vic/adapter/MediaPlayer.java
+++
@@ -1,6 +0,0 @@
-package com.vic.adapter;
-
-public interface MediaPlayer {
-
-	public void play(String type, String name);
-}
