855f31a921dfded791409f2502a1b78c8b5af603
Faithlmy <lmengyy@126.com>
2018-09-24 17:24:46 +0800

    Singleton

---
+++ src/com/vic/Singleton/SingletonPatternDemo.java
@@ -0,0 +1,12 @@
+package com.vic.Singleton;
+
+public class SingletonPatternDemo {
+
+	public static void main(String[] args) {
+		//获取唯一可用的对象
+	      SingleObject object = SingleObject.getInstance();
+	 
+	      //显示消息
+	      object.showMessage();
+	}
+}
